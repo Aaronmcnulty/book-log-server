4d40585224ffe6c144cd27c15cc297590f7dda71
malcomcrab <malcomcrabesq@outlook.com>
2025-03-05 12:49:16 +0000

    Feature: Adds jwt verification and protected route demo

--- App.js
+++ App.js
@@ -18,11 +18,25 @@ app.use(passport.initialize());
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
-app.get('/login-success', (req,res) => {
-  const user = req.user
-  jwt.sign({user}, 'secret', (err, token) => {
-    res.json({token})
+app.get('/login-success', (req, res) => {
+  const user = req.user.username
+  jwt.sign({ user }, 'secretKey', (err, token) => {
+    res.json({ token })
+  })
 })
+
+app.post('/post', verifyToken,(req, res) => {
+  jwt.verify(req.token, 'secretKey',  {expiresIn: '2h'}, (err, authdata) => {
+    if (err) {
+      res.sendStatus(403)
+    } else {
+      res.json(
+        {
+          message: 'Posty',
+          authdata
+        })
+    }
+  })
 })
 
 app.post(
@@ -31,8 +45,21 @@ app.post(
     successRedirect: "/login-success",
     failureRedirect: "/"
   })
- 
 );
+
+
+function verifyToken (req, res, next) {
+  const bearerHeader = req.headers['authorization']
+  if(typeof bearerHeader !== 'undefined'){
+    const bearer = bearerHeader.split(' ')
+
+    const bearerToken = bearer[1]
+    req.token = bearerToken
+    next()
+  } else {
+    res.sendStatus(403)
+  }
+}
 
 const PORT = 3000;
 app.listen(PORT, () => {
